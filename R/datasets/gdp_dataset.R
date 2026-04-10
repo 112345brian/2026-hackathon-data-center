63d88ee0b3cd141c66e0dcbf79ed8711f1f63489
bri <11creepman@gmail.com>
2026-04-10 17:24:08 -0400

    fix gdp

--- R/datasets/gdp_dataset.R
+++ R/datasets/gdp_dataset.R
@@ -1,10 +1,17 @@
 # gdp_dataset
 
-gdp_data <- read_csv(here("data", "raw", "CAGDP1__ALL_AREAS_2001_2024.csv"), show_col_types = FALSE) |>
-  rename(geoid = "GeoFIPS", yr_2023 = "2023") |>
-  select(geoid, Description, yr_2023) |>
-  mutate(geoid = str_remove_all(geoid, "'")) |>
-  pivot_wider(names_from = "Description", values_from = "yr_2023") |>
-  select(-matches("^NA$")) |>
-  clean_names() |>
-  mutate(geoid = str_pad(geoid, width = 5, pad = "0"))
+gdp_raw <- read_csv(
+  here("data", "raw", "CAGDP1__ALL_AREAS_2001_2024.csv"),
+  col_types = cols(.default = col_character()),
+  progress = FALSE,
+  show_col_types = FALSE
+)
+
+gdp_data <- gdp_raw |>
+  transmute(
+    geoid = str_pad(str_remove_all(GeoFIPS, "'"), width = 5, pad = "0"),
+    Description,
+    value_2023 = as.numeric(`2023`)
+  ) |>
+  pivot_wider(names_from = Description, values_from = value_2023) |>
+  clean_names()
