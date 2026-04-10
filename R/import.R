a51c86b22a4c98b6a31cb952141ec5636a3ff14f
laurenwilmoth <lwilmot1@jh.edu>
2026-04-10 15:01:38 -0400

    Update import.R

--- R/import.R
+++ R/import.R
@@ -5,3 +5,5 @@ library(here)
 source(here("R", "imp3", "import_imp3_and_fips.R"))
 source(here("R", "census", "import_census.R")) # depends on import_census_API_setup.R
 source(here("R", "energy_burden", "import_energy_burden.R"))
+source(here("R", "gdp", "import_gdp.R"))
+source(here("R", "urbanicity", "import_urbanicity.R"))
