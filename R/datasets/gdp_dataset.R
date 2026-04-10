9f49d9307ba5a6c77ba9b0eb378d1ba99864f570
bri <11creepman@gmail.com>
2026-04-10 17:31:11 -0400

    fix the gdpdsdasdf

--- R/datasets/gdp_dataset.R
+++ R/datasets/gdp_dataset.R
@@ -1,8 +1,20 @@
 # gdp_dataset
 
-gdp_raw <- read_csv(
+gdp_lines <- read_lines(
   here("data", "raw", "CAGDP1__ALL_AREAS_2001_2024.csv"),
+  locale = locale(encoding = "latin1"),
+  progress = FALSE
+)
+
+note_idx <- which(str_detect(gdp_lines, '^"Note:'))
+if (length(note_idx) > 0) {
+  gdp_lines <- gdp_lines[seq_len(note_idx[1] - 1)]
+}
+
+gdp_raw <- read_csv(
+  I(paste(gdp_lines, collapse = "\n")),
   col_types = cols(.default = col_character()),
+  locale = locale(encoding = "latin1"),
   progress = FALSE,
   show_col_types = FALSE
 )
@@ -11,7 +23,10 @@ gdp_data <- gdp_raw |>
   transmute(
     geoid = str_pad(str_remove_all(GeoFIPS, "'"), width = 5, pad = "0"),
     Description,
-    value_2023 = as.numeric(`2023`)
+    value_2023 = parse_number(
+      `2023`,
+      na = c("", "NA", "(NA)", "(D)", "(L)", "(S)", "(C)", "(NM)", "NM")
+    )
   ) |>
   pivot_wider(names_from = Description, values_from = value_2023) |>
   clean_names()
